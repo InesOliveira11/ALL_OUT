9732c104a6362b5be3d1e91826ac543390251160
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-22 17:32:29 +0000

    question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_162358) do
+ActiveRecord::Schema.define(version: 2021_11_22_172704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.string "answer_1"
+    t.string "answer_2"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
