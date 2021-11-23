cba4e28042427f97c381c958a193c83139794a8a
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-23 12:02:58 +0000

    activity controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_175420) do
-
+ActiveRecord::Schema.define(version: 2021_11_22_180953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +44,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_175420) do
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
-  
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "date"
@@ -99,18 +97,13 @@ ActiveRecord::Schema.define(version: 2021_11_22_175420) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "activity_tags", "activities"
   add_foreign_key "activity_tags", "questions"
-
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
-  
   add_foreign_key "bookings", "activities"
   add_foreign_key "bookings", "users"
-  
   add_foreign_key "favourites", "activities"
   add_foreign_key "favourites", "users"
-  
   add_foreign_key "reviews", "bookings"
 end
