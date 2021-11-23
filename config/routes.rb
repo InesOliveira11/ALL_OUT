3c2d0734d3ab14bf3c4fb8f03d089a87537422e4
Ines de Sousa Oliveira <inessoliveir@gmail.com>
2021-11-23 15:12:01 +0000

    reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :questions
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities
   resources :users
   resources :activities, only: [:show] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show]
+  resources :reviews
 end
