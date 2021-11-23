88b828cbaa54b8518f21a7834a2983d5a559d01b
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-23 15:07:39 +0000

    favoutires controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :activities
-  resources :users
-
+  resources :activities, only: %i[index show] do
+    resources :favourites, only: %i[index create destroy]
+  end
+  resources :users, only: [:show]
 end
