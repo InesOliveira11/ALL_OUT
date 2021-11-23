7702f75f8bbcf28b5987efda401922211eb54326
Esther Carminatti <yunalesca002@gmail.com>
2021-11-23 14:02:19 +0000

    new method implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :activities
   resources :users
-  resources :questions
+  resources :questions do
+    resources :answers, only: [:new, :create]
+  end
 
 end
