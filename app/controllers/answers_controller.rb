34d67af4aa5798e015fb5bf14d19c73f370c9eb7
Esther Carminatti <yunalesca002@gmail.com>
2021-11-23 15:10:54 +0000

    answers controller (new and create), views done

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,4 +4,21 @@ class AnswersController < ApplicationController
     @answer = Answer.new
     @user = current_user
   end
+
+  def create
+    @answer = Answer.new(answer_params)
+    @question = Question.find(params[:question_id])
+    @answer.question = @question
+    @answer.user = current_user
+    if @answer.save!
+      redirect_to new_question_answer_path(params[:answer][:next])
+    else
+      render :new
+    end
+  end
+  private
+
+  def answer_params
+    params.require(:answer).permit(:selected_answer, :question_id)
+  end
 end
