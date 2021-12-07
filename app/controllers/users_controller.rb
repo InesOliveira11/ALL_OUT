491ecf017696dd1dd5e3034e97dd34bc83925f1c
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-12-07 14:27:13 +0000

    cleaning the code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,35 +17,6 @@ class UsersController < ApplicationController
     @current_bookings = @bookings.select { |b| b.date >= Date.today }
   end
 
-  # def new
-  #   @user = User.new
-  # end
-
-  # def edit
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to @user, notice: 'User was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def update
-  #   if @user.update(user_params)
-  #     redirect_to @user, notice: 'User was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def destroy
-  #   @user.destroy
-  #   redirect_to user_url, notice: 'User was successfully destroyed.'
-  # end
-
   private
 
   def set_user
